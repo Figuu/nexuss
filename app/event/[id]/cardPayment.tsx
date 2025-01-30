2870b5c9d8d01e7073c462e5d0b859b3d050055d
Figuu <david.calvi.18@gmail.com>
2025-01-30 14:46:35 -0400

    card qr payment update

--- app/event/[id]/cardPayment.tsx
+++ app/event/[id]/cardPayment.tsx
@@ -3,6 +3,7 @@ import { ScrollView, Text, Touchable, TouchableOpacity } from "react-native";
 import Modal from "react-native-modal";
 import { SafeAreaView, View } from "react-native";
 import WebView from "react-native-webview";
+import axios from "axios";
 
 interface PaymentViewProps {
   visible: boolean;
@@ -10,6 +11,30 @@ interface PaymentViewProps {
 }
 
 const CardPaymentView: React.FC<PaymentViewProps> = ({ visible, onClose }) => {
+  const generateCardPayment = async () => {
+    try {
+      const response = await axios.post("https://yopago.com.bo/pay/qr/generateUrl", {
+        companyCode: "ATPG-P8V8-22TK-H43G", // Código de la empresa
+        codeTransaction: "3aA", // Código de transacción
+        urlSuccess: "https://exito.com.bo", // URL de éxito
+        urlFailed: "https://falla.com.bo", // URL de fallo
+        billName: fullName, // Nombre del comprador
+        billNit: "123456789", // NIT (opcional)
+        email: email, // Correo electrónico
+        generateBill: "1", // Generar factura
+        concept: `Pago para ${event.name}`, // Concepto del pago
+        currency: "BOB", // Moneda
+        amount: ticket.price * quantity, // Monto total
+        messagePayment: "Gracias por tu compra!", // Mensaje de pago
+        codeExternal: "", // Código externo (opcional)
+      });
+      setCardVisible(true); // Abre el modal de pago con tarjeta
+    } catch (error) {
+      setError("Error al generar el pago con tarjeta");
+    }
+  };
+
+
   return (
     <Modal
       animationIn="slideInUp"
